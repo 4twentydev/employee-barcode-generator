668bfd574075de83a672bb2213c869716a3b4bf8
4twentydev <4twenty.dev@gmail.com>
2025-12-31 06:40:30 -0700

    Fix DATABASE_URL typing

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,9 +1,11 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
-if (!process.env.DATABASE_URL) {
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
   throw new Error("DATABASE_URL is not set");
 }
 
-const sql = neon(process.env.DATABASE_URL);
+const sql = neon(databaseUrl);
 export const db = drizzle(sql);
